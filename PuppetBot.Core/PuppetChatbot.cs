f025fee8ea4b0e56a2aa58e87f8e0b4cfd02bfee
Dowol <dowol1212@gmail.com>
2023-02-25 23:37:47 +0900

    2302252337

---
+++ PuppetBot.Core/PuppetChatbot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client;
+
+namespace PuppetBot
+{
+    public class PuppetChatbot
+    {
+        protected TwitchClient Twitch { get; private set; }
+
+
+
+        public PuppetChatbot()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ PuppetBot.Server/Services/ChattingService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PuppetBot.Server.Hubs;
+
+namespace PuppetBot.Server.Services;
+
+public class ChattingService : IHostedService
+{
+    private IHubContext<ChattingHub> hubContext;
+
+    public ChattingService(IHubContext<ChattingHub> hubContext)
+    {
+        this.hubContext = hubContext;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await hubContext.Clients.All.SendAsync("ReceiveChat");
+        
+
+        await Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+    }
+}
